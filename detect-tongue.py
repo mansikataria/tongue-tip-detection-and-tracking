55ba156c021406864ce232c5417710de68c9725d
mansikataria <mkataria920@gmail.com>
2022-01-07 14:11:06 +0530

    modifications to detect_tongue

--- detect-tongue.py
+++ detect-tongue.py
@@ -1,7 +1,18 @@
 import cv2 as cv
+from imutils import face_utils
 import numpy as np
 import operator
 import math
+from matplotlib import pyplot as plt
+
+import argparse
+import imutils
+import dlib
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--shape-predictor", required=False,
+	help="path to facial landmark predictor", default="shape_predictor_68_face_landmarks.dat")
+args = vars(ap.parse_args())
 
 # The video feed is read in as
 # a VideoCapture object
@@ -24,7 +35,7 @@ prev_gray = cv.cvtColor(first_frame, cv.COLOR_BGR2GRAY)
 mask = np.zeros_like(first_frame)
 
 # Sets image saturation to maximum
-mask[..., 1] = 255
+mask[..., 1] = 0
 i=0
 while(cap.isOpened()):
 	
@@ -32,7 +43,9 @@ while(cap.isOpened()):
 	# the frame, frame = the current frame being
 	# projected in the video
 	ret, frame = cap.read()
-	
+	if not ret:
+		print('No frames grabbed!')
+		break
 	# Opens a new window and displays the input
 	# frame
 	cv.imshow("input", frame)
@@ -41,22 +54,97 @@ while(cap.isOpened()):
 	# only converted the first frame to grayscale
 	gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 	
-	# Calculates dense optical flow by Farneback method
-	flow = cv.calcOpticalFlowFarneback(prev_gray, gray,
-									None,
-									0.5, 3, 15, 3, 5, 1.2, 0)
+	#get mouth part
 	
+	detector = dlib.get_frontal_face_detector()
+	predictor = dlib.shape_predictor(args["shape_predictor"])
+	
+	rects = detector(gray, 1)
+
+	# loop over the face detections
+	for (i, rect) in enumerate(rects):
+		# determine the facial landmarks for the face region, then
+		# convert the landmark (x, y)-coordinates to a NumPy array
+		shape = predictor(gray, rect)
+		shape = face_utils.shape_to_np(shape)
+		# loop over the face parts individually
+		for (name, (i, j)) in face_utils.FACIAL_LANDMARKS_IDXS.items():
+			if(name == "mouth"):
+		    	# clone the original image so we can draw on it, then
+		    	# display the name of the face part on the image
+				clone = gray.copy()
+				cv.putText(clone, name, (10, 30), cv.FONT_HERSHEY_SIMPLEX,
+				0.7, (0, 0, 255), 2)
+		    	# loop over the subset of facial landmarks, drawing the
+		    	# specific face part
+				for (x, y) in shape[i:j]:
+					cv.circle(clone, (x, y), 1, (0, 0, 255), -1)
+            		# extract the ROI of the face region as a separate image
+					(x, y, w, h) = cv.boundingRect(np.array([shape[i:j]]))
+					roi = gray[y:y + h, x:x + w]
+					roi = imutils.resize(roi, width=250, inter=cv.INTER_CUBIC)
+					print("roi shape")
+					print(roi.shape)
+		    		# show the particular face part
+            		# cv.imshow("ROI", roi)
+				break
+
+	rects = detector(prev_gray, 1)
+
+	# loop over the face detections
+	for (i, rect) in enumerate(rects):
+		# determine the facial landmarks for the face region, then
+		# convert the landmark (x, y)-coordinates to a NumPy array
+		shape = predictor(prev_gray, rect)
+		shape = face_utils.shape_to_np(shape)
+		# loop over the face parts individually
+		for (name, (i, j)) in face_utils.FACIAL_LANDMARKS_IDXS.items():
+			if(name == "mouth"):
+		    	# clone the original image so we can draw on it, then
+		    	# display the name of the face part on the image
+				clone = prev_gray.copy()
+				cv.putText(clone, name, (10, 30), cv.FONT_HERSHEY_SIMPLEX,
+				0.7, (0, 0, 255), 2)
+		    	# loop over the subset of facial landmarks, drawing the
+		    	# specific face part
+				for (x, y) in shape[i:j]:
+					cv.circle(clone, (x, y), 1, (0, 0, 255), -1)
+            		# extract the ROI of the face region as a separate image
+					(x, y, w, h) = cv.boundingRect(np.array([shape[i:j]]))
+					prev_roi = prev_gray[y:y + h, x:x + w]
+					prev_roi = imutils.resize(prev_roi, width=250, inter=cv.INTER_CUBIC)
+					print("prev roi shape")
+					print(prev_roi.shape)
+		    		# show the particular face part
+            		# cv.imshow("ROI", roi)
+				break
+
+        
+	# Calculates dense optical flow by Farneback method
+	flow = cv.calcOpticalFlowFarneback(prev_roi, roi, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+
 	# Computes the magnitude and angle of the 2D vectors
 	magnitude, angle = cv.cartToPolar(flow[..., 0], flow[..., 1])
-	index, value = max(enumerate(magnitude[0]), key=operator.itemgetter(1))
-	# print(magnitude.shape)
+	# print(magnitude)
 	# print(flow.shape)
-	print(flow[:,:,0])
-	if(value > 6):
-		print(index)
-		color = (0, 0, 255)
-		image = cv.circle(frame, (math.floor(flow[index,0,0]), math.ceil(flow[0,index,1])), 30, color, thickness=-1)
-		cv.imwrite("frames/"+str(i)+".jpg", image)
+	# print(flow[:,:,0])
+	# print(max(magnitude[0]))
+	index, value = max(enumerate(magnitude[0]), key=operator.itemgetter(1))
+	print(i)
+	print(index)
+	print(value)
+	value = max(magnitude[0])
+	cv.imwrite("frames/"+str(i)+".jpg", frame)
+	# if(value > 6):
+	# 	print(max(magnitude[0]))
+	# 	print(magnitude)
+	# 	index, value = max(enumerate(magnitude[0]), key=operator.itemgetter(1))
+	# 	print(index)
+	# 	# print(value)
+	# 	# print(index)
+	# 	color = (0, 0, 255)
+	# 	image = cv.circle(frame, (math.floor(flow[index,index,0]), math.ceil(flow[index,index,1])), 5, color, thickness=-1)
+	# 	cv.imwrite("frames/"+str(i)+".jpg", frame)
 
 	# Sets image hue according to the optical flow
 	# direction
@@ -73,7 +161,7 @@ while(cap.isOpened()):
 	i=i+1
 	cv.imshow("dense optical flow", rgb)
 	# Resize the Window
-	cv.resizeWindow("dense optical flow", 10, 10)
+	# cv.resizeWindow("dense optical flow", 10, 10)
 	# Updates previous frame
 	prev_gray = gray
 	
